cf73d2115bb8e4c19883ebe8db185bbafd05de31
yanmiller-rus <75687988+yanmiller-rus@users.noreply.github.com>
2020-12-14 21:18:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ d["car3"] = "Toyota"
 d["car4"] = "Aston Martin"
 d["car5"] = "Range Rover"
 d["car6"] = "Jaguar"
-print("Всего машин в магазине: ", len(d))
-print("Авто-словрь", d.values())
+eval('''print("Всего машин в магазине: ", len(d))''')
+eval('''print("Авто-словрь", d.values())''')
 
 # d2 = {a: a ** 2 for a in range(1, 10)}
 # d2.update(d)
 # print("Словарь №2", d2)
 
-print("Цена BMW: 5 000 000")
-print("Цена Porsche: 3 500 000")
-print("Цена Toyota: 1 500 000")
-print("Цена Aston Martin: 17 490 000")
-print("Цена Range Rover: 4 000 000")
-print("Цена Jaguar: 6 400 000 ")
+eval('''print("Цена BMW: 5 000 000")''')
+eval('''print("Цена Porsche: 3 500 000")''')
+eval('''print("Цена Toyota: 1 500 000")''')
+eval('''print("Цена Aston Martin: 17 490 000")''')
+eval('''print("Цена Range Rover: 4 000 000")''')
+eval('''print("Цена Jaguar: 6 400 000 ")''')
 
-print("Ваша сумма денег: ")
+eval('''print("Ваша сумма денег: ")''')
 your_balance = int(input())
-print("Ваша сумма денег: %s " % your_balance)
+eval('''print("Ваша сумма денег: %s " % your_balance)''')
 
 if your_balance >= 2500000:
-    print("Все хорошо, вам хватает средств")
+    eval('''print("Все хорошо, вам хватает средств для продолжения покупки")''')
 else:
-    print("У вас не хватает средств!")
-    print("Вы не можете продолжить покупку!")
-    print("Пожалуйста, покиньте консольный центр")
+    eval('''print("У вас не хватает средств!")''')
+    eval('''print("Вы не можете продолжить покупку!")''')
+    eval('''print("Пожалуйста, покиньте консольный центр")''')
 
-print("Введите цену машины")
+eval('''print("Введите цену машины")''')
 price_of_car = int(input())
-print("Цена выбранного автомобиля: %s " % price_of_car)
+eval('''print("Цена выбранного автомобиля: %s " % price_of_car)''')
 
-print("Укажите ваше имя")
+eval('''print("Укажите ваше имя")''')
 name = str(input())
-print("Ваше имя: %s " % name)
+eval('''print("Ваше имя: %s " % name)''')
 
 
 rest_money = your_balance - price_of_car
-print("Покупка успешна завершена :) . Ждем вас в следующий раз")
-print("Ваш текущий баланс: %s " % rest_money)
+eval('''print("Покупка успешна завершена :) . Ждем вас в следующий раз")''')
+eval('''print("Ваш текущий баланс: %s " % rest_money)''')
