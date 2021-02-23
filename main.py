167b8c7504ce6c999f89194e6017a2d3edd094f8
yanmiller-rus <75687988+yanmiller-rus@users.noreply.github.com>
2021-02-23 16:44:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-name_of_company = "CAR SHOP on console"
-print("%s" % name_of_company)
-location = "Moscow, Russia"
-print("Место расположения: %s " % location)
-
-print("Доступный ассортимент")
-d = dict(car1="BMW - 5 000 000 руб", car2="Porshe - 3 500 000 руб")
-d["car3"] = "Toyota - 1 500 000 руб"
-d["car4"] = "Aston Martin - 17 490 000 руб"
-d["car5"] = "Range Rover - 4 000 000 руб"
-d["car6"] = "Jaguar - 6 400 000 руб"
-eval('''print("Всего машин в салоне: ", len(d))''')
-eval('''print("Авто-словрь", d.values())''')
+def info():
+    name_of_company = "CAR SHOP ON CONSOLE"
+    the_location = "Moscow, Russia"
+    print("Название компании:", name_of_company)
+    print("Местоположение:", the_location)
 
 
-eval('''print("Ваша сумма денег: ")''')
-your_balance = int(input())
-eval('''print("Ваша сумма денег: %s " % your_balance)''')
-
-if your_balance >= 2500000:
-    eval('''print("Все хорошо, вам хватает средств для продолжения покупки")''')
-else:
-    eval('''print("У вас не хватает средств!")''')
-    eval('''print("Вы не можете продолжить покупку!")''')
-    eval('''print("Пожалуйста, покиньте консольный центр")''')
-
-eval('''print("Введите цену машины")''')
-price_of_car = int(input())
-eval('''print("Цена выбранного автомобиля: %s " % price_of_car)''')
-
-eval('''print("Укажите ваше имя")''')
-name = str(input())
-eval('''print("Ваше имя: %s " % name)''')
+def get_available_cars():
+    print("Доступный ассортимент")
+    cars = {
+        'Toyota': '1 500 000 руб', 'Aston Martin': '17 490 000 руб', 'Range Rover': '4 000 000 руб', 'Jaguar': '6 400 000 руб'
+           }
+    print("Всего машин в салоне: ", len(cars))
+    print("Авто-словрь")
+    for key, values in cars.items():
+        print(key, values)
 
 
-rest_money = your_balance - price_of_car
-eval('''print("Покупка успешна завершена :) . Ждем вас в следующий раз")''')
-eval('''print("Ваш текущий баланс: %s " % rest_money)''')
+def buy_car():
+    print("Ваша сумма денег: ")
+    your_balance = int(input())
+    print("Ваша сумма денег: %s " % your_balance)
+
+    if your_balance >= 1500000:
+        print("Все хорошо, вам хватает средств для продолжения покупки")
+        print("Введите цену машины")
+        price_of_car = int(input())
+        print("Цена выбранного автомобиля: %s " % price_of_car)
+
+        print("Укажите ваше имя")
+        name = str(input())
+        print("Ваше имя: %s " % name)
+
+        rest_money = your_balance - price_of_car
+        print("Покупка успешна завершена :) . Ждем вас в следующий раз")
+        print("Ваш текущий баланс: %s " % rest_money)
+
+    else:
+        print("У вас не хватает средств!")
+        print("Вы не можете продолжить покупку!")
+        print("Пожалуйста, покиньте консольный центр!")
+
+
+info()
+print("===================================")
+get_available_cars()
+buy_car()
