b1161527f2dd22411e944561361ccad51ef11780
Yan Miller <75687988+yanmiller-rus@users.noreply.github.com>
2021-04-12 00:41:34 +0300

    Add files via upload

---
+++ balance.py
@@ -0,0 +1,14 @@
+
+
+def increase_balance():
+    print('Введите сумму для пополнения счета')
+    print('==================================')
+    money = int(input())
+    self.balance += money
+
+
+def take_off():
+    print('Введите сумму для снятия')
+    print('========================')
+    taking = int(input())
+    self.balance -= taking
--- main.py
+++ main.py
@@ -1,47 +1,82 @@
-def info():
-    name_of_company = "CAR SHOP ON CONSOLE"
-    the_location = "Moscow, Russia"
-    print("Название компании:", name_of_company)
-    print("Местоположение:", the_location)
+class Shop:
+
+    def __init__(self, name, balance):
+        self.name = name
+        print('Введите свой баланс')
+        self.balance = int(input())
+
+    @staticmethod
+    def info():
+        name_of_company = "MLR Technology"
+        the_location = "Moscow, Russia"
+        print("Название компании:", name_of_company)
+        print("Местоположение:", the_location)
+
+    @staticmethod
+    def get_available_cars():
+        print("Доступный ассортимент")
+        cars = {
+            'Toyota': '1 500 000 руб', 'Aston Martin': '17 490 000 руб', 'Range Rover': '4 000 000 руб',
+            'Jaguar': '6 400 000 руб'
+        }
+
+        print("Всего моделей в салоне: ", len(cars))
+        print("Авто-словрь")
+
+        for key, values in cars.items():
+            print(key, values)
 
 
-def get_available_cars():
-    print("Доступный ассортимент")
-    cars = {
-        'Toyota': '1 500 000 руб', 'Aston Martin': '17 490 000 руб', 'Range Rover': '4 000 000 руб', 'Jaguar': '6 400 000 руб'
-           }
-    print("Всего машин в салоне: ", len(cars))
-    print("Авто-словрь")
-    for key, values in cars.items():
-        print(key, values)
+    def check_my_balance(self):
+        print(self.balance)
 
 
-def buy_car():
-    print("Ваша сумма денег: ")
-    your_balance = int(input())
-    print("Ваша сумма денег: %s " % your_balance)
+    def increase_balance(self):
+        print('Введите сумму для пополнения счета')
+        print('==================================')
+        money = int(input())
+        self.balance += money
 
-    if your_balance >= 1500000:
-        print("Все хорошо, вам хватает средств для продолжения покупки")
-        print("Введите цену машины")
-        price_of_car = int(input())
-        print("Цена выбранного автомобиля: %s " % price_of_car)
+    def take_off(self):
+        print('Введите сумму для снятия')
+        print('========================')
+        taking = int(input())
+        self.balance -= taking
 
-        print("Укажите ваше имя")
-        name = str(input())
-        print("Ваше имя: %s " % name)
+    def buy_car(self):
 
-        rest_money = your_balance - price_of_car
-        print("Покупка успешна завершена :) . Ждем вас в следующий раз")
-        print("Ваш текущий баланс: %s " % rest_money)
+        if self.balance >= 1500000:
+            print("Все хорошо, вам хватает средств для продолжения покупки")
+            print("Введите цену машины")
+            price_of_car = int(input())
+            print("Цена выбранного автомобиля: %s " % price_of_car)
 
-    else:
-        print("У вас не хватает средств!")
-        print("Вы не можете продолжить покупку!")
-        print("Пожалуйста, покиньте консольный центр!")
+            print("Укажите ваше имя")
+            name = str(input())
+            print("Ваше имя: %s " % name)
+
+            rest_money = self.balance - price_of_car
+            print("Покупка успешна завершена :) . Ждем вас в следующий раз")
+            print("Ваш текущий баланс: %s " % rest_money)
+
+        else:
+            print("У вас не хватает средств!")
+            print("Пожалуйста, пополните ваш счет для продолжения покупки")
+            client_1.increase_balance()
+            client_1.check_my_balance()
+            self.balance = True
+            if self.balance == True:
+                client_1.buy_car()
 
 
-info()
-print("===================================")
-get_available_cars()
-buy_car()
+client_1 = Shop('Yan Miller', balance='')
+client_1.info()
+print('==============================')
+client_1.get_available_cars()
+print('==============================')
+print('Для покупки нажмите 1')
+c = int(input())
+if c == 1:
+    client_1.buy_car()
+else:
+    print('Ошибка')
